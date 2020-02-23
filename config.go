6dc73e998bcbeeb3c04f74402207838d197266f0
jinzhongmin <jinzm1993@163.com>
2020-02-23 09:47:11 +0800

    修改

--- config.go
+++ config.go
@@ -1,309 +1,386 @@
 package config
 
 import (
-	"io/ioutil"
+	"bytes"
+	"log"
 	"os"
-	"strconv"
+	"regexp"
 	"strings"
 )
 
-//Config ..
-type Config struct {
-	lines []*Line
+type IfDo struct {
+	seg string
+	fn  func(l *Line)
 }
 
-//View ..
-type View struct {
-	lines []*Line
+type Seg struct {
+	word []string
 }
 
-//Line ..
 type Line struct {
-	tab  int
-	args []string
+	indent int
+	word   []string
 }
 
-func read(file string) string {
-	fs, err := os.Open(file)
-	if err != nil {
-	}
-	bt, err := ioutil.ReadAll(fs)
-	if err != nil {
-	}
-	return string(bt)
+type View struct {
+	body []*Line
 }
-func clean(text string) string {
-	text = strings.Replace(text, "\r\n[", ";;;[;;;", -1)
-	text = strings.Replace(text, "\r\n#", ";;;#;;;", -1)
 
-	for i := 20; i > 0; i-- {
-		text = strings.Replace(text, "\r\n"+blank(i), ";;;"+strconv.Itoa(i)+";", -1)
-		if i > 1 {
-			text = strings.Replace(text, blank(i), " ", -1)
+type Config struct {
+	body []*Line
+}
+
+func ReadLine(line string) Line {
+	reg := regexp.MustCompilePOSIX("^[ ]*")
+	indent := reg.FindString(line)
+	l := strings.Replace(line, indent, "", 1)
+	s := new(string)
+	s = &l
+	for {
+		if strings.Index(*s, "  ") > 0 {
+			l := strings.Replace(*s, "  ", " ", -1)
+			s = &l
+		} else {
+			break
+		}
+	}
+	return Line{len(indent), strings.Split(*s, " ")}
+}
+
+func NewSeg(seg string) Seg {
+	reg := regexp.MustCompilePOSIX("^[ ]*")
+	indent := reg.FindString(seg)
+	l := strings.Replace(seg, indent, "", 1)
+	s := new(string)
+	s = &l
+	for {
+		if strings.Index(*s, "  ") > 0 {
+			l := strings.Replace(*s, "  ", " ", -1)
+			s = &l
+		} else {
+			break
+		}
+	}
+	return Seg{strings.Split(*s, " ")}
+}
+
+func NewView() *View {
+	v := new(View)
+	v.body = make([]*Line, 0)
+	return v
+}
+
+func Switch(i ...interface{}) []IfDo {
+	id := make([]IfDo, 0)
+	if len(i)%2 != 0 {
+		return id
+	}
+	for ii, o := range i {
+		if ii%2 == 0 {
+			seg, ok1 := o.(string)
+			fn, ok2 := i[ii+1].(func(l *Line))
+			if ok1 && ok2 {
+				id = append(id, IfDo{seg, fn})
+			}
+
 		}
 	}
 
-	text = strings.Replace(text, "\r\n", "", -1)
-
-	text = strings.Replace(text, ";;;[;;;", "\r\n[", -1)
-	text = strings.Replace(text, ";;;#;;;", "\r\n#", -1)
-
-	for i := 20; i > 0; i-- {
-		text = strings.Replace(text, ";;;"+strconv.Itoa(i)+";", "\r\n"+blank(i), -1)
-	}
-
-	return text
+	return id
 }
-func blank(num int) string {
-	str := ""
-	for i := 0; i < num; i++ {
-		str += " "
+
+func (l *Line) Copy() *Line {
+	_l := new(Line)
+	_l.word = make([]string, 0)
+	_l.word = append(_l.word, l.word...)
+	return _l
+}
+func (l *Line) String() string {
+	indent := ""
+	for i := 1; i < l.indent; i++ {
+		indent += " "
+	}
+	str := indent
+	for _, s := range l.word {
+		str += " " + s
 	}
 	return str
 }
 
-//New ..
-func New(fs string) *Config {
-	config := new(Config)
-	text := clean(read(fs))
-
-	lines := strings.Split(text, "\r\n")
-	for i := range lines {
-		config.lines = append(config.lines, NewLine(lines[i]))
+func (l *Line) IfBegin(seg string, fn func(l *Line)) bool {
+	s := NewSeg(seg)
+	if len(l.word) < len(s.word) {
+		return false
 	}
-
-	return config
-}
-
-//Lines ..
-func (config *Config) Lines(match []string) []*Line {
-	lines := make([]*Line, 0)
-
-	for i := range config.lines {
-		line := config.lines[i]
-
-		if len(line.args) < len(match) {
+	for i, w := range s.word {
+		if w == "*" {
 			continue
 		}
-
-		flag := true
-		for ii := range match {
-			if line.args[ii] == match[ii] {
-				continue
-			}
-			flag = false
-		}
-
-		if flag == true {
-			lines = append(lines, line)
+		if l.word[i] != w {
+			return false
 		}
 	}
-
-	return lines
+	fn(l)
+	return true
 }
 
-//ViewsAuto ..
-func (config *Config) ViewsAuto(match []string) []*View {
-	views := make([]*View, 0)
-
-	for i := 0; i < len(config.lines); i++ {
-		lines := make([]*Line, 0)
-		line := config.lines[i]
-
-		if len(line.args) < len(match) {
-			continue
-		}
-
-		flag := true
-		for ii := range match {
-			if line.args[ii] == match[ii] {
-				continue
-			}
-			flag = false
-			break
-		}
-
-		if flag == true { //开始匹配
-			lines = append(lines, line)
-			i++
-
-			tabs := line.Tabs()
-
-			for ; i < len(config.lines); i++ {
-				if config.lines[i].Tabs() > tabs {
-					lines = append(lines, config.lines[i])
-				} else {
-					view := new(View)
-					view.lines = lines
-					views = append(views, view)
-					i = i - 1
-					break
-				}
-			}
-		}
+func (l *Line) IfInclude(word string, fn func(l *Line)) {
+	if strings.Index(l.String(), word) > -1 {
+		fn(l)
 	}
-	return views
 }
 
-//Views ..
-func (config *Config) Views(begin []string, end []string) []*View {
-	views := make([]*View, 0)
-
-	for i := 0; i < len(config.lines); i++ {
-		lines := make([]*Line, 0)
-		line := config.lines[i]
-		if len(line.args) < len(begin) {
-			continue
-		}
-
-		flag := true
-		for ii := range begin {
-			if line.args[ii] == begin[ii] {
-				continue
-			}
-			flag = false
-			break
-		}
-
-		if flag == true { //开始匹配
-			lines = append(lines, line)
-
-			for i = i + 1; i < len(config.lines); i++ {
-				line = config.lines[i]
-
-				if len(line.args) < len(end) {
-					lines = append(lines, line)
-					continue
-				}
-
-				for ii := range end {
-					if line.args[ii] == end[ii] {
-						continue
-					}
-					flag = false
-					break
-				}
-				lines = append(lines, config.lines[i])
-
-				if flag == true {
-					view := new(View)
-					view.lines = lines
-					views = append(views, view)
-					i = i - 1
-					break
-				}
-
-				flag = true
-			}
-		}
+func (l *Line) GetIndex(index int) string {
+	if index >= len(l.word) {
+		return ""
 	}
-	return views
+	return l.word[index]
 }
 
-//LinesAll ..
-func (view *View) LinesAll() []*Line {
-	return view.lines
-}
-
-//Lines ..
-func (view *View) Lines(match []string) []*Line {
-	lines := make([]*Line, 0)
-
-	for i := range view.lines {
-		line := view.lines[i]
-
-		if len(line.args) < len(match) {
-			continue
-		}
-
-		flag := true
-		for ii := range match {
-			if line.args[ii] == match[ii] {
-				continue
+func (l *Line) GetNext(word string) string {
+	for i, w := range l.word {
+		if w == word {
+			if i+1 < len(l.word) {
+				return l.word[i+1]
 			}
-			flag = false
-		}
-
-		if flag == true {
-			lines = append(lines, line)
-		}
-	}
-
-	return lines
-}
-
-//NewLine ..
-func NewLine(line string) *Line {
-	tab := 0
-	for i := 20; i > 0; i-- {
-		index := strings.Index(line, blank(i))
-		if index == 0 {
-			tab = i
-			break
-		}
-	}
-
-	line = strings.Replace(line, blank(tab), "", 1)
-	args := strings.Split(line, " ")
-
-	l := new(Line)
-	l.tab = tab
-	l.args = args
-
-	return l
-}
-
-//NextStr ..
-func (line *Line) NextStr(arg string) string {
-	for i := range line.args {
-		if line.args[i] == arg && len(line.args) > i+1 {
-			return line.args[i+1]
+			return ""
 		}
 	}
 	return ""
 }
 
-//NextInt ..
-func (line *Line) NextInt(arg string) int {
-	for i := range line.args {
-		if line.args[i] == arg && len(line.args) > i+1 {
-			r, err := strconv.Atoi(line.args[i+1])
-			if err != nil {
-				return -1
-			}
-			return r
+func (l *Line) DeleteWord(word string) {
+	for i, w := range l.word {
+		if w == word {
+			_w := make([]string, 0)
+			_w = append(_w, l.word[:i]...)
+			_w = append(_w, l.word[i+1:]...)
+			l.word = _w
 		}
 	}
-	return -1
+
 }
 
-//Indexs ..
-func (line *Line) Indexs(indexs []int) []string {
-	args := make([]string, 0)
-	for i := range indexs {
-		args = append(args, line.args[indexs[i]])
+func (l *Line) DeleteWordNext(word string) {
+	for i, w := range l.word {
+		if w == word && i+1 < len(l.word) {
+			_w := make([]string, 0)
+			_w = append(_w, l.word[:i+1]...)
+			_w = append(_w, l.word[i+2:]...)
+			l.word = _w
+		}
 	}
 
-	return args
 }
 
-//IndexStr ..
-func (line *Line) IndexStr(index int) string {
-	return line.args[index]
+func (l *Line) DeleteWordByIndex(index int) {
+	if index < len(l.word) {
+		w := make([]string, 0)
+		w = append(w, l.word[:index]...)
+		w = append(w, l.word[index+1:]...)
+		l.word = w
+	}
 }
 
-//IndexInt ..
-func (line *Line) IndexInt(index int) int {
-	i, _ := strconv.Atoi(line.args[index])
-	return i
+func (l *Line) ModifWord(old string, new string) {
+	for i, w := range l.word {
+		if w == old {
+			end := l.word[i+1:]
+			l.word = append(l.word[:i], new)
+			l.word = append(l.word, end...)
+			return
+		}
+	}
 }
 
-//Len ..
-func (line *Line) Len() int {
-	return len(line.args)
+func (l *Line) ModifWordNext(word string, new string) {
+	for i, w := range l.word {
+		if w == word && i+1 < len(l.word) {
+			end := l.word[i+2:]
+			l.word = append(l.word[:i+1], new)
+			l.word = append(l.word, end...)
+			return
+		}
+	}
 }
 
-//Tabs ..
-func (line *Line) Tabs() int {
-	return line.tab
+func (l *Line) ModifWordByIndex(index int, new string) {
+	if index < len(l.word) {
+		end := l.word[index+1:]
+		l.word = append(l.word[:index], new)
+		l.word = append(l.word, end...)
+	}
+}
+
+func (v *View) Copy() *View {
+	_v := new(View)
+	_v.body = make([]*Line, 0)
+	v.RangeLine(func(l *Line) {
+		_v.body = append(_v.body, l.Copy())
+	})
+
+	return _v
+}
+
+func (v *View) String() string {
+	str := ""
+	for _, l := range v.body {
+		str += l.String() + "\n"
+	}
+	return str
+}
+
+func (v *View) AppendLine(l *Line) {
+	v.body = append(v.body, l)
+}
+
+func (v *View) GetLineByIndex(index int) *Line {
+	return v.body[index]
+}
+
+func (v *View) RangeLine(fn func(l *Line)) {
+	for i, _ := range v.body {
+		fn(v.body[i])
+	}
+}
+
+func (v *View) RangeLineIfBegin(word string, fn func(l *Line)) {
+	for i, _ := range v.body {
+		v.body[i].IfBegin(word, fn)
+	}
+}
+
+func (v *View) RangeLineIfInclude(word string, fn func(l *Line)) {
+	for i, _ := range v.body {
+		v.body[i].IfInclude(word, fn)
+	}
+}
+
+func (v *View) RangeLineSwitchBegin(sw []IfDo) {
+	for i, _ := range v.body {
+		for _, ii := range sw {
+			v.body[i].IfBegin(ii.seg, ii.fn)
+		}
+	}
+}
+
+func (v *View) RangeLineSwitchInclude(sw []IfDo) {
+	for i, _ := range v.body {
+		for _, ii := range sw {
+			v.body[i].IfInclude(ii.seg, ii.fn)
+		}
+	}
+}
+
+func LoadConfig(path string) *Config {
+	fs, err := os.Open(path)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	buf := bytes.Buffer{}
+	buf.ReadFrom(fs)
+
+	cfg := buf.String()
+	cfgls := strings.Split(cfg, "\r\n")
+
+	ls := make([]*Line, 0)
+	for _, l := range cfgls {
+		_l := ReadLine(l)
+		ls = append(ls, &_l)
+	}
+
+	c := new(Config)
+	c.body = ls
+	return c
+}
+
+func (c *Config) GetViews(seg string) []*View {
+	view := make([]*View, 0)
+
+	lv := new(View)
+	li := 0
+	for _, l := range c.body {
+		if l.IfBegin(seg, func(l *Line) {
+			if len(lv.body) > 0 {
+				lv.body[0].IfBegin(seg, func(l *Line) {
+					view = append(view, lv)
+				})
+			}
+
+			lv = new(View)
+			lv.body = append(lv.body, l)
+			li = l.indent
+		}) {
+			continue
+		}
+
+		if l.indent < li {
+			if len(lv.body) > 0 {
+				lv.body[0].IfBegin(seg, func(l *Line) {
+					view = append(view, lv)
+				})
+			}
+
+			lv = new(View)
+			continue
+		}
+		if l.indent >= li {
+			li = l.indent
+		}
+		lv.body = append(lv.body, l)
+	}
+
+	return view
+}
+
+func (c *Config) RangeViews(seg string, fn func(v *View)) {
+	vs := c.GetViews(seg)
+	for _, v := range vs {
+		fn(v)
+	}
+}
+
+func (c *Config) RangeLinesBegin(seg string, fn func(l *Line)) {
+	for _, l := range c.body {
+		l.IfBegin(seg, fn)
+	}
+}
+
+func (c *Config) RangeLinesInclude(seg string, fn func(l *Line)) {
+	for _, l := range c.body {
+		l.IfInclude(seg, fn)
+	}
+}
+
+func (c *Config) RangeLineSwitchBegin(sw []IfDo) {
+	for i, _ := range c.body {
+		for _, ii := range sw {
+			c.body[i].IfBegin(ii.seg, ii.fn)
+		}
+	}
+}
+
+func (c *Config) RangeLineSwitchInclude(sw []IfDo) {
+	for i, _ := range c.body {
+		for _, ii := range sw {
+			c.body[i].IfInclude(ii.seg, ii.fn)
+		}
+	}
+}
+
+func (c *Config) GetLinesViewBegin(seg string) *View {
+	v := new(View)
+	c.RangeLinesBegin(seg, func(l *Line) {
+		v.body = append(v.body, l)
+	})
+	return v
+}
+
+func (c *Config) GetLinesViewInclude(seg string) *View {
+	v := new(View)
+	c.RangeLinesInclude(seg, func(l *Line) {
+		v.body = append(v.body, l)
+	})
+	return v
 }
